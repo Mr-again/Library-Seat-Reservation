4156a2fe73bec1ebfbce0824c127cca11de9b164
zhaone <smzhgle@gmail.com>
2018-12-05 17:27:27 +0800

    zh

--- handler.py
+++ handler.py
@@ -2,5 +2,5 @@ from seatRes import webRequests
 import warnings
 
 warnings.filterwarnings('ignore')
-webRequests.reserve('xxxxxxxxxxxxx', 'xxxxxx', 2685, 1380, 1410)
+webRequests.reserve('000000000', '898989', 2685, 1380, 1410)
 
