eaa71fdef066a9082a76a72df785235407de223d
zengcheng.johnny <zengcheng.johnny@bytedance.com>
2018-12-07 10:52:38 +0800

    feat: cancel
    Change-Id: Ieb0533a301121537cc63adc6384e049c050fabc0

--- seatRes/webRequests.py
+++ seatRes/webRequests.py
@@ -4,11 +4,19 @@ from urllib import parse
 
 
 def reserve(username, password, seat, start, end):
-    print('reserve my seat')
+    print('!!!!!!!!reserve my seat!!!!!!!!')
     cookie, token_login = get_cookie()
     log_in(username, password, cookie, token_login)
     token_res = get_login_page(cookie)
-    make_res(cookie, token_res, seat, start, end)
+    res_id = make_res(cookie, token_res, seat, start, end)
+    return res_id
+
+
+def cancel(username, password, seat_id):
+    print('!!!!!!!!cancel my seat!!!!!!!!')
+    cookie, token_login = get_cookie()
+    log_in(username, password, cookie, token_login)
+    cancel_res(cookie, seat_id)
 
 
 def get_cookie():
@@ -111,4 +119,29 @@ def make_res(cookie, token, seat, start, end):
             'appAuthKey': 'a109981dd38540d5b20b4af760d7f6f1'}
     r = requests.post('https://seat.lib.whu.edu.cn/selfRes', verify=False, headers=headers, data=data)
     print('code: ', r.status_code)
+    seq_id = etree.HTML(r.text).xpath("//dd/text()")[0]
+    if len(seq_id) != 10:
+        print('res fail')
+        return None
+    res_id = '47'+seq_id.split('-')[2]+'3'+seq_id.split('-')[1]
+    print(res_id)
+    return res_id
 
+
+def cancel_res(cookie, seat_id):
+    print('===cancel res===')
+    if seat_id is None:
+        print('no seat to cancel')
+        return
+    headers = {'Host': 'seat.lib.whu.edu.cn',
+               'Connection': 'keep-alive',
+               'Upgrade-Insecure-Requests': '1',
+               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) '
+                             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+               'Referer': 'https://seat.lib.whu.edu.cn/login?targetUri=%2F',
+               'Accept-Encoding': 'gzip, deflate, br',
+               'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+               'Cookie': cookie}
+    r = requests.get('https://seat.lib.whu.edu.cn/reservation/cancel/'+seat_id, headers=headers, verify=False, allow_redirects=False)
+    print('code: ', r.status_code)
