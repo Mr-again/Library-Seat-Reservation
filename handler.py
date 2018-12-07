3fe570bc6e6b9ab5d872328a0ec80fc8f2bc8db0
zengcheng.johnny <zengcheng.johnny@bytedance.com>
2018-12-07 15:57:54 +0800

    feat: usr name
    Change-Id: Iccbc7df948a6344ab439d42c0a5adb7e0c699719

--- handler.py
+++ handler.py
@@ -2,6 +2,6 @@ from seatRes import webRequests
 import warnings
 
 warnings.filterwarnings('ignore')
-seat_id = webRequests.reserve('2015301500269', '084818', 2685, 1380, 1410)
+seat_id = webRequests.reserve('xxx', 'xxx', 2685, 1380, 1410)
 if seat_id is not None:
-    webRequests.cancel('2015301500269', '084818', seat_id)
+    webRequests.cancel('xxx', 'xxx', seat_id)
