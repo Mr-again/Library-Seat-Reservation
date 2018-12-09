d8eb2d8d2e3e3afc678575aa16cef00626116a1e
zengcheng.johnny <zengcheng.johnny@bytedance.com>
2018-12-09 14:53:57 +0800

    feature: add multi req
    Change-Id: I0503ca2c16da9ff4271631aa917da809c77d03cd

--- handler.py
+++ handler.py
@@ -2,8 +2,10 @@ from seatRes import webRequests
 from service import snatch
 import warnings
 
-warnings.filterwarnings('ignore')
-# seat_id = webRequests.reserve('xxx', 'xxx', 2685, 1380, 1410)
-# if seat_id is not None:
-#     webRequests.cancel('xxx', 'xxx', seat_id)
-snatch.snatch_seat('xxx', 'xxx', 2659)
+
+if __name__ == '__main__':
+    warnings.filterwarnings('ignore')
+    # seat_id = webRequests.reserve('xxx', 'xxx', 2685, 1380, 1410)
+    # if seat_id is not None:
+    #     webRequests.cancel('xxx', 'xxx', seat_id)
+    snatch.snatch_seat('xxx', 'xxx', 2659)
--- service/snatch.py
+++ service/snatch.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 from seatRes import webRequests
+import multiprocessing
+import warnings
+
+
+result = None
 
 
 def check_time():
     cur_time = datetime.now()
-    des_time = cur_time.replace(hour=11, minute=28, second=0, microsecond=0)
+    des_time = cur_time.replace(hour=14, minute=50, second=0, microsecond=0)
     delta = des_time-cur_time
     skip_seconds = int(delta.total_seconds())
     if skip_seconds == 0:
@@ -13,13 +18,32 @@ def check_time():
         return False
 
 
-def snatch_seat(username, password, seat):
+def snatch_seat(lock, username, password, seat):
+    global result
     while True:
         if check_time():
             for i in range(3):
                 res_id = webRequests.reserve(username, password, seat, 1260, 1350)
                 if res_id is not None:
                     print('预约成功!res_id为:'+res_id+',time = '+str(datetime.now()))
-                    return res_id
+                    if res_id is not None:
+                        lock.acquire()
+                        result = res_id
+                        lock.release()
+                        print('预约序列存储成功!')
+                    return True
             print('预约失败！,time = '+str(datetime.now()))
-            return None
+            return False
+
+
+def multi_snatch(username, password, seat):
+    lock = multiprocessing.Lock()
+    for i in range(10):
+        p = multiprocessing.Process(target=snatch_seat, args=(lock, username, password, seat))
+        p.daemon = False
+        p.start()
+
+
+if __name__ == '__main__':
+    warnings.filterwarnings('ignore')
+    multi_snatch('2015301500270', '178511', 2659)
