c57dee5f9b548ab279645f046f98ec4dd159d67d
zengcheng.johnny <zengcheng.johnny@bytedance.com>
2018-12-10 21:29:14 +0800

    feat: add comfig
    Change-Id: I704b153c01b42ff4c4e07888a0251f53d86fd478

--- service/snatch.py
+++ service/snatch.py
@@ -1,13 +1,24 @@
 import datetime
 from seatRes import webRequests
 import multiprocessing
+import configparser
 import warnings
-import time
+
+
+warnings.filterwarnings('ignore')
+conf_ini = configparser.ConfigParser()
+conf_ini.read('../conf.ini')
+hour = conf_ini.getint("snatch", "pre_res_hour")
+minute = conf_ini.getint("snatch", "pre_res_minute")
+second = conf_ini.getint("snatch", "pre_res_second")
+microsecond = conf_ini.getint("snatch", "pre_res_microsecond")
+users_ini = configparser.ConfigParser()
+users_ini.read('../users.ini')
 
 
 def check_time():
     cur_time = datetime.datetime.now()
-    des_time = cur_time.replace(hour=14, minute=19, second=0, microsecond=0)
+    des_time = cur_time.replace(hour=hour, minute=minute, second=second, microsecond=microsecond)
     delta = des_time-cur_time
     skip_seconds = int(delta.total_seconds())
     if skip_seconds == 0:
@@ -16,20 +27,20 @@ def check_time():
         return False
 
 
-def snatch_seat(res, lock, username, password, seat):
+def snatch_seat(lock, username, password, seat):
     while True:
         if check_time():
             for i in range(3):
-                res_id = webRequests.reserve(username, password, seat, 1260, 1350, False)
+                res_id = webRequests.reserve(username, password, seat, 1320, 1350, False)
                 if res_id is not None:
                     print('预约成功!res_id为:'+res_id+',time = '+str(datetime.datetime.now()))
                     if res_id is not None:
                         lock.acquire()
-                        res[username] = {'res_id': res_id,
-                                         'password': password,
-                                         'seat_id': seat,
-                                         'start': 1290,
-                                         'end': 1350}
+                        users_ini.set('2015301500270', 'res_id', str(res_id))
+                        users_ini.set('2015301500270', 'seat_id', str(seat))
+                        users_ini.set('2015301500270', 'start', str(1290))
+                        users_ini.set('2015301500270', 'end', str(1350))
+                        users_ini.write(open('../users.ini', 'w'))
                         lock.release()
                         print('预约序列存储成功!')
                     return True
@@ -37,20 +48,15 @@ def snatch_seat(res, lock, username, password, seat):
             return False
 
 
-def multi_snatch(res, username, password, seat):
+def multi_snatch(username, password, seat):
     lock = multiprocessing.Lock()
     for i in range(10):
-        p = multiprocessing.Process(target=snatch_seat, args=(res, lock, username, password, seat))
+        p = multiprocessing.Process(target=snatch_seat, args=(lock, username, password, seat))
         p.daemon = False
         p.start()
-        # TODO How to use join here
 
 
 if __name__ == '__main__':
-    warnings.filterwarnings('ignore')
     manager = multiprocessing.Manager()
     result = manager.dict()
-    multi_snatch(result, 'xxx', 'xxx', 12333)
-    while True:
-        print(result)
-        time.sleep(1)
+    multi_snatch('xxx', 'xxx', 12333)
